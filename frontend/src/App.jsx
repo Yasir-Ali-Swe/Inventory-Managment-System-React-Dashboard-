7e4019d7dca2748be08bc3349b42f13f67b51c07
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-06-29 23:47:33 +0500

    setup Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Layout from './Components/Layout.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from './Pages/Products.jsx';
+import Orders from './Pages/Orders.jsx';
+import Salesman from './Pages/Salesman.jsx';
+import Sales from './Pages/Sales.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+          <Route index  element={<Products />} />
+          <Route path="Orders" element={<Orders />} />
+          <Route path="Salesman" element={<Salesman />} />
+          <Route path="Sales" element={<Sales />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
-export default App
+export default App;
