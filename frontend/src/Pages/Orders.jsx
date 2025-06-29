0433d7c5b6b5dc09689f35ea45e7f015a21c49bb
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-06-29 23:46:03 +0500

    All Pages

---
+++ frontend/src/Pages/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div>
+        <h1 className='text-center text-2xl font-bold text-textColor'>Orders</h1>
+    </div>
+  )
+}
+
+export default Orders;
---
+++ frontend/src/Pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>
+        <h1 className='text-center text-2xl font-bold text-textColor'>Products</h1>
+    </div>
+  )
+}
+
+export default Products;
