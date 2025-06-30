87639a4d9c2f144b74c7f0de755d6a1530794d48
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-06-30 18:04:09 +0500

    update the routes

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -6,13 +6,15 @@ const Sidebar = () => {
     const links=[
         {name:"Products",path:"/"},
         {name:"Orders",path:"/Orders"},
-        {name:"Salesman",path:"/Salesman"},
         {name:"Sales",path:"/Sales"},
+        {name:"Cashier",path:"/Cashier"},
+         {name:"Profile",path:"/Profile"},
     ]
     return (
         <div className='w-1/5 bg-mainForground h-full'>
-            <div className='border-textColor border-b-2 h-14 flex items-center'>
+            <div className='border-mainBg border-b-4 h-18 flex flex-col justify-center'>
                 <h1 className='pl-12 text-2xl font-bold text-textColor'>Sidebar</h1>
+                <h1 className='pl-12 text-[12px] font-semibold text-textColor'>Inventory Managment System</h1>
             </div>
             <div className='mt-4'>
                 <ul className='flex flex-col gap-4'>
