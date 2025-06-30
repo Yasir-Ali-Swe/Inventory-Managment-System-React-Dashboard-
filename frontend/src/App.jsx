ce642bd79bccf7e6357962dcab1f689ef9c375a0
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-06-30 18:08:04 +0500

    Update the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import Layout from './Components/Layout.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Products from './Pages/Products.jsx';
 import Orders from './Pages/Orders.jsx';
-import Salesman from './Pages/Salesman.jsx';
+import Cashier from './Pages/Cashier.jsx';
 import Sales from './Pages/Sales.jsx';
+import Profile from './Pages/Profile.jsx';
 
 const App = () => {
   return (
@@ -13,8 +14,9 @@ const App = () => {
           <Route path="/" element={<Layout />} >
           <Route index  element={<Products />} />
           <Route path="Orders" element={<Orders />} />
-          <Route path="Salesman" element={<Salesman />} />
+          <Route path="Cashier" element={<Cashier />} />
           <Route path="Sales" element={<Sales />} />
+          <Route path="Profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
