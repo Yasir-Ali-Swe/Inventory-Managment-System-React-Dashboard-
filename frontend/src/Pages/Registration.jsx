5bee05095d8ab06a3dd41f7839dda6887e6eb723
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-01 21:45:12 +0500

    Add a Registration Page

---
+++ frontend/src/Pages/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <div>
+        <h1 className='text-center text-2xl font-bold text-textColor'>Registration</h1>
+    </div>
+  )
+}
+
+export default Registration;
