97807c5bdc20dfc7cd6b711f1d9f3e0eb29e9c8c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-10 20:47:41 +0500

    Remove unused components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,9 @@
-import Layout from './Components/Layout.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Products from './Pages/Products.jsx';
-import Orders from './Pages/Orders.jsx';
-import Cashier from './Pages/Cashier.jsx';
-import Sales from './Pages/Sales.jsx';
-import Profile from './Pages/Profile.jsx';
-
+import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} >
-          <Route index  element={<Products />} />
-          <Route path="Orders" element={<Orders />} />
-          <Route path="Cashier" element={<Cashier />} />
-          <Route path="Sales" element={<Sales />} />
-          <Route path="Profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <h1 className='text-center text-white text-3xl font-bold underline bg-purple-950 py-3'>Yasir Ali</h1>
     </div>
   )
 }
