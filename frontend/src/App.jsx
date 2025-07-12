33800402f12fe33e8d25839a756315d21dd3bbf4
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-12 11:14:54 +0500

    Entry Point

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Login, Signup, Categories, Salesman, Analytics, Reports, Dashboard ,Products } from './pages/index.jsx';
+import { Login, Signup, Categories, Salesman, Analytics, Reports, Dashboard, Products, Profile, Invoice } from './pages/index.jsx';
 import { Layout } from './components/index.jsx';
 
 const App = () => {
@@ -12,8 +12,10 @@ const App = () => {
           <Route path='categories' element={<Categories />} />
           <Route path='salesman' element={<Salesman />} />
           <Route path='analytics' element={<Analytics />} />
+          <Route path='invoice' element={<Invoice />} />
+          <Route path='profile' element={<Profile />} />
           <Route path='reports' element={<Reports />} />
-        </Route>  
+        </Route>
         <Route path='login' element={<Login />} />
         <Route path='signup' element={<Signup />} />
       </Routes>
